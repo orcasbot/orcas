636a4ea63a1e79b286a9a0b7d9f23b6e85bcd266
ExitWise <exitwise@users.noreply.github.com>
2026-05-21 05:11:28 +0700

    fix: migration for eth_buy_amount_usd column rename

---
+++ prisma/migrations/20260521000000_fix_eth_buy_amount/migration.sql
@@ -0,0 +1,4 @@
+-- Rename eth_buy_amount to eth_buy_amount_usd and fix type
+ALTER TABLE "user_settings" RENAME COLUMN "eth_buy_amount" TO "eth_buy_amount_usd";
+ALTER TABLE "user_settings" ALTER COLUMN "eth_buy_amount_usd" TYPE DECIMAL(18,6);
+ALTER TABLE "user_settings" ALTER COLUMN "eth_buy_amount_usd" SET DEFAULT 5;
